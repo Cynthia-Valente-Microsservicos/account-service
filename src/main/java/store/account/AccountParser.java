016d3ada00ddf36e86c9c8571e482578a94e55b6
CYNahko <cynthiayasutake@gmail.com>
2026-05-13 12:44:15 -0300

    copy-paste from classes

---
+++ src/main/java/store/account/AccountParser.java
@@ -0,0 +1,31 @@
+package store.account;
+
+import java.util.List;
+
+import store.account.AccountIn;
+import store.account.AccountOut;
+
+public class AccountParser {
+
+    public static AccountOut to(Account in){
+        return in == null ? null :
+            AccountOut.builder()
+                .id(in.id())
+                .name(in.name())
+                .email(in.email())
+                .build();
+    }
+
+    public static List<AccountOut> to(List<Account> in) {
+        return in.stream().map(AccountParser::to).toList();
+    }
+
+    public static Account to(AccountIn in){
+        return in == null ? null:
+            Account.builder()
+                .name(in.name())
+                .email(in.email())
+                .password(in.password())
+                .build();
+    }   
+}
---
+++ src/main/java/store/account/AccountRepository.java
@@ -0,0 +1,13 @@
+package store.account;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends CrudRepository<AccountModel, String> {
+
+    public Optional<AccountModel> findByEmailAndPasswordSha256(String email, String sha256);
+
+}
